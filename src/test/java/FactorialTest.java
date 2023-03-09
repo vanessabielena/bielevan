1b1b23a8da563a31a9c9bee2f74a8345efbe8e83
vanessabielena <vanessabielena@gmail.com>
2023-03-09 14:58:55 +0100

    Added test for factorial method

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -2,9 +2,9 @@ import cz.cvut.fel.ts1.Bielena;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class BielenaTest {
+public class FactorialTest {
     @Test
-    public void factorialTest_() {
+    public void factorialTest() {
         Assertions.assertEquals(120, Bielena.factorialRecursive(5));
     }
 }
