51da9d44bf270a29c884f5338301244638fc632e
vanessabielena <101097681+vanessabielena@users.noreply.github.com>
2023-03-19 00:27:35 +0100

    Domaci ukol

---
+++ bielevan/src/main/java/cz/cvut/fel/ts1/MetodaCoPocitaFaktorial.java
@@ -0,0 +1,24 @@
+package cz.cvut.fel.ts1;
+
+public class MetodaCoPocitaFaktorial {
+
+    public static int factorialRecursive(int n){
+        if(n <= 1){
+            return 1;
+        }
+        else{
+            return n*factorialRecursive(n-1);
+        }
+    }
+
+    public static int factorialIterative(int n){
+        int result=1,i=1;
+        while(i<=n){
+            result=result*i;
+            i++;
+        }
+
+        return result;
+    }
+
+}
